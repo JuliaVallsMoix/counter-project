0c5a535b43f5fd2724ccdaad120e691ca5bea4aa
Júlia Valls Moix <juliavalls20@gmail.com>
2023-05-03 12:12:58 +0200

    Done

--- js/app.js
+++ js/app.js
@@ -5,28 +5,28 @@ let counter = 0;
 // Seleccionar el botón ADD COUNT y añadir un listener para el evento 'click'. Cada vez que el usuario haga click en el botón, incrementamos en 1 la variable de estado. 
 // Luego, seleccionar el nodo donde esta la información textual y sobrescribirlo con el valro de la variable
 
+// Cuando usamos mucho el mismo nodo más de una vez en la función, guárdalo en una variable para simplificar el código:
+
+const counterElemement = document.querySelector('#counter');
+
+function updateCounter(value) {
+    counterElemement.innerHTML = value;
+    if (value > 0) {
+        counterElemement.style.color = 'green';
+    } else if (value < 0) {
+        counterElemement.style.color = 'red';
+    } else {
+        counterElemement.style.color = '#333333';
+    };
+};
+
 document.querySelector('.nextBtn').addEventListener('click', () => {
     counter++;
-    document.querySelector('#counter').innerHTML = counter; 
-    if (counter > 0) {
-        document.querySelector('#counter').style.color = 'green';
-    } else if (counter < 0) {
-        document.querySelector('#counter').style.color = 'red';
-    } else {
-        document.querySelector('#counter').style.color = 'grey';
-    };
-    
-    
+    updateCounter(counter); 
 });
 
 document.querySelector('.prevBtn').addEventListener('click', () => {
     counter--;
-    document.querySelector('#counter').innerHTML = counter;
-    if (counter < 0) {
-        document.querySelector('#counter').style.color = 'red';
-    } else if (counter > 0) {
-        document.querySelector('#counter').style.color = 'green';
-    } else {
-        document.querySelector('#counter').style.color = 'grey';
-    };
+    updateCounter(counter); 
 });
+
